c857fd21960d0db912d0ef4f52e5ae9ce08e608c
Rajagopalan-Gangadharan <g.raju2000@gmail.com>
2020-09-24 23:33:43 +0530

    [WIP] Linux Components(Remove debug)

--- Source/CoreUI/Linux/linuxmainapp.cpp
+++ Source/CoreUI/Linux/linuxmainapp.cpp
@@ -25,17 +25,11 @@
 
 
 #include "linuxmainapp.h"
-#include <iostream>
 
 LinuxMainApp::LinuxMainApp()
     : DConsoleUI()
 {
-    std::cout << "Console App";
 }
 void LinuxMainApp::StartApp()
 {
-    for (int i = 0; i < 100000; ++i)
-    {
-        std::cout << "Hello world";
-    }
 }
