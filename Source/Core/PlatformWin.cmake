e488b55dd9f1c8a33084b1bac666f92e37639980
Rajagopalan-Gangadharan <g.raju2000@gmail.com>
2021-01-03 10:41:56 +0530

    Old version purged

--- Source/Core/PlatformWin.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 3.0)
-
-
-add_library(WinCore 
-Windows/wincore.cpp
-)
--- Source/CoreUI/PlatformLinux.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 3.0)
-
-
-add_library(LinuxUI 
-Linux/linuxmainapp.cpp
-)
